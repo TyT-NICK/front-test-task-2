6e1465c7cab258f6961c347306620a705acef64a
TyT-NICK <artem99k14@gmail.com>
2021-07-04 22:23:47 +0800

    added signout page

---
+++ src/pages/SignOutPage.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
+import useAuth from '../hooks/auth.hook'
+
+const SignOutPage = () => {
+  const { signOut } = useAuth()
+
+  useEffect(() => {
+    signOut()
+  }, [])
+
+  return <Redirect to="auth" />
+}
+
+export default SignOutPage
