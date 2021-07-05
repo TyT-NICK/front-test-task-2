607cc5b4d84445201d20be1356ce9794bb7e7871
TyT-NICK <artem99k14@gmail.com>
2021-07-05 15:30:20 +0800

    finished favorite management

--- src/components/FavoriteForm.js
+++ src/components/FavoriteForm.js
@@ -1,18 +1,10 @@
-import { Card, Form, Input, Button, Select, Slider, Row, Col, InputNumber } from 'antd'
-import { useForm } from 'antd/lib/form/Form'
-import { useMemo, useState } from 'react'
+import { Form, Input, Select, Slider, Row, Col, InputNumber } from 'antd'
+import { useState } from 'react'
 
 const { Option } = Select
 
-const FavoriteForm = (props) => {
-  const { type } = props
-  const formTitle = useMemo(() => {
-    if (!type) return ''
-    return type.charAt(0).toUpperCase() + type.slice(1), [type]
-  }, [type])
-
-  const [form] = useForm()
-  const [maxResultsValue, setMaxResultsValue] = useState(12)
+const FavoriteForm = ({ onChange, form }) => {
+  const [maxResultsValue, setMaxResultsValue] = useState(form.getFieldValue('maxResults'))
 
   const onFinish = (values) => {
     console.log(values)
@@ -20,55 +12,66 @@ const FavoriteForm = (props) => {
 
   const onMaxResultsChange = (value) => {
     setMaxResultsValue(value)
+    onChange({ ...form.getFieldsValue(), maxResults: value })
+  }
+
+  const valuesChangeHandler = (_, allValues) => {
+    onChange(allValues)
   }
 
   return (
-    <Card title={`${formTitle} запрос`} className="form form-favorite">
-      <Form form={form} layout={'vertical'} onFinish={onFinish}>
-        <Form.Item name="searchString" label="Запрос">
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="name"
-          label="Название"
-          rules={[{ required: true, message: 'Необходимо указать название' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item name="order" label="Сортировать по">
-          <Select defaultValue="relevance">
-            <Option value="relevance">Релевантности</Option>
-            <Option value="date">Дате</Option>
-            <Option value="title">Наименованию</Option>
-            <Option value="rating">Рейтингу</Option>
-            <Option value="viewCount">Числу просмотров</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item name="maxResults" label="Максимальное количество">
-          <Row gutter={16}>
-            <Col flex="4">
-              <Slider
-                defaultValue={12}
-                min={1}
-                max={50}
-                step={1}
-                onChange={onMaxResultsChange}
-                value={typeof maxResultsValue === 'number' ? maxResultsValue : 0}
-              />
-            </Col>
-            <Col flex="1">
-              <InputNumber min={1} max={50} value={maxResultsValue} onChange={onMaxResultsChange} />
-            </Col>
-          </Row>
-        </Form.Item>
-        <Form.Item>
+    <Form
+      form={form}
+      className="form form-favorite"
+      layout={'vertical'}
+      onFinish={onFinish}
+      onValuesChange={valuesChangeHandler}
+    >
+      <Form.Item name="searchString" label="Запрос">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="title"
+        label="Название"
+        rules={[{ required: true, message: 'Необходимо указать название' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item name="order" label="Сортировать по">
+        <Select>
+          <Option value="relevance">Релевантности</Option>
+          <Option value="date">Дате</Option>
+          <Option value="title">Наименованию</Option>
+          <Option value="rating">Рейтингу</Option>
+          <Option value="viewCount">Числу просмотров</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item name="maxResults" label="Максимальное количество">
+        <Row gutter={16}>
+          <Col flex="4">
+            <Slider
+              defaultValue={12}
+              min={1}
+              max={50}
+              step={1}
+              onChange={onMaxResultsChange}
+              value={typeof maxResultsValue === 'number' ? maxResultsValue : 0}
+            />
+          </Col>
+          <Col flex="1">
+            <InputNumber min={1} max={50} value={maxResultsValue} onChange={onMaxResultsChange} />
+          </Col>
+        </Row>
+      </Form.Item>
+      {/* <Form.Item>
           <Button type="primary" htmlType="submit">
             {formTitle}
           </Button>
           <Button htmlType="button">Отмена</Button>
-        </Form.Item>
-      </Form>
-    </Card>
+        </Form.Item> */}
+    </Form>
+    // <Card title={`${formTitle} запрос`} >
+    // </Card>
   )
 }
 
--- src/pages/FavoritePage.js
+++ src/pages/FavoritePage.js
@@ -1,3 +1,135 @@
-const FavoritePage = () => {}
+import { Layout, List, Modal } from 'antd'
+import { useForm } from 'antd/lib/form/Form'
+import { useContext, useEffect, useState } from 'react'
+import FavoriteForm from '../components/FavoriteForm'
+import PageHeader from '../components/PageHeader'
+import useAuth from '../hooks/auth.hook'
+import { Link } from 'react-router-dom'
+import { FavoriteContext, FAVORITE_ACTION_TYPES } from '../reducers/favorite.reducer'
+
+const { Content } = Layout
+const { confirm } = Modal
+
+const ListItem = ({ item, onEdit, onDelete }) => {
+  const [modalVisible, setModalVisible] = useState(false)
+  const [changedItem, setChangedItem] = useState({ ...item })
+  const [urlStr, setUrlStr] = useState(new URLSearchParams())
+
+  useEffect(() => {
+    urlStr.set('q', changedItem.searchString)
+    urlStr.set('order', changedItem.order)
+    urlStr.set('maxResults', changedItem.maxResults)
+
+    setUrlStr(urlStr)
+  }, [changedItem])
+
+  const [form] = useForm()
+  useEffect(() => {
+    form.setFieldsValue({ ...item })
+  }, [])
+
+  const deleteHandler = (e) => {
+    e.preventDefault()
+    onDelete()
+  }
+
+  const editHandler = (e) => {
+    e.preventDefault()
+
+    setModalVisible(true)
+  }
+
+  const closeModalHandler = () => {
+    setModalVisible(false)
+    setChangedItem({ ...item })
+    setTimeout(() => {
+      form.setFieldsValue({ ...item })
+    }, 250)
+  }
+
+  const okModalHandler = () => {
+    setModalVisible(false)
+    onEdit(changedItem)
+  }
+
+  return (
+    <List.Item className="favorite-item">
+      <Link to={`/search?${urlStr.toString()}`} style={{ display: 'contents' }}>
+        <h2 className="favorite-item__title">{item.title}</h2>
+        <span className="favorite-item__extra">
+          {`${item.searchString}, ${item.order}, ${item.maxResults}`}
+        </span>
+      </Link>
+      <div className="favorite-item__actions">
+        <a href="/" className="favorite-item__edit" onClick={editHandler}>
+          Изменить
+        </a>
+        <a href="/" className="favorite-item__delete" onClick={deleteHandler}>
+          Удалить
+        </a>
+      </div>
+      <Modal
+        title="Изменить запрос"
+        closable={false}
+        maskClosable
+        visible={modalVisible}
+        onCancel={closeModalHandler}
+        onOk={okModalHandler}
+        okText="Сохранить запрос"
+        cancelText="Отмена"
+      >
+        <FavoriteForm form={form} onChange={(values) => setChangedItem({ ...values })} />
+      </Modal>
+    </List.Item>
+  )
+}
+
+const FavoritePage = () => {
+  const { login } = useAuth()
+  const fContext = useContext(FavoriteContext)
+
+  const itemDeleteHandler = (index) => {
+    confirm({
+      title: 'Удалить избранный запрос?',
+      maskClosable: true,
+      okText: 'Удалить',
+      cancelText: 'Отмена',
+      onOk: () => {
+        fContext.dispatch({
+          type: FAVORITE_ACTION_TYPES.DELETE_FAVORITE,
+          payload: { login, index },
+        })
+      },
+    })
+  }
+
+  const itemEditHandler = (index, item) => {
+    fContext.dispatch({
+      type: FAVORITE_ACTION_TYPES.EDIT_FAVORITE,
+      payload: { login, index, item },
+    })
+  }
+
+  return (
+    <Layout className="page">
+      <PageHeader />
+      <Content>
+        <div className="page__content">
+          <h1>Избранное</h1>
+          <List
+            dataSource={fContext.state[login] || []}
+            renderItem={(item, i) => (
+              <ListItem
+                item={item}
+                onEdit={(item) => itemEditHandler(i, item)}
+                onDelete={() => itemDeleteHandler(i)}
+              />
+            )}
+          />
+        </div>
+      </Content>
+    </Layout>
+  )
+}
 
 export default FavoritePage
